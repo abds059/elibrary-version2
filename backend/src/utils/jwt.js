4735976b7a2b96f71b4731b0501b2cb96251cbc5
Abdur Rehman Siddiqui <abdurrehmans059@gmail.com>
2026-05-12 01:43:04 +0500

    hrhfbgg

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -15,7 +15,7 @@ function issueToken(res, payload) {
   res.cookie(COOKIE_NAME, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
   });
 
@@ -38,7 +38,7 @@ function clearToken(res) {
   res.clearCookie(COOKIE_NAME, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
   });
 }
 
